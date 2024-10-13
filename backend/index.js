4f7128fb19f55b222ddd5bb89cc3fcb089326f20
temitopeohassan <temitope.hassan.2@gmail.com>
2024-10-13 15:10:03 +0100

    add port number

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,8 @@ const db = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'booksology'
+  database: process.env.DB_NAME || 'booksology',
+  port: process.env.DB_PORT || '3306',
 });
 
 // Middleware to ensure connection is established
